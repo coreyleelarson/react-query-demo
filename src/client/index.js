a0df36a4832f9e7ef97c86e7307c0eb3b602aec9
Corey Larson <coreyleelarson@gmail.com>
2020-09-30 11:42:16 -0500

    Adds basic routing.

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,27 @@
 import CssBaseline from '@material-ui/core/CssBaseline';
 import React from 'react';
 import { render } from 'react-dom';
-import App from 'client/app';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import ReactQueryApp from 'client/variants/react-query';
+import ReduxApp from 'client/variants/redux';
 
 render(
-  <>
+  <BrowserRouter>
     <CssBaseline />
-    <App />
-  </>,
+    <nav>
+      <ul>
+        <li>
+          <Link to="/redux">Redux</Link>
+        </li>
+        <li>
+          <Link to="/react-query">React Query</Link>
+        </li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/react-query" element={<ReactQueryApp />} />
+      <Route path="/redux" element={<ReduxApp />} />
+    </Routes>
+  </BrowserRouter>,
   document.querySelector('#root')
 );
